78da0809d982477f4cbdcb12ba669166baa646d2
paulo <744964089@qq.com>
2019-03-22 18:15:10 +0800

    添加手机短信服务

---
+++ pau-rest/src/main/java/com/soft/ware/rest/modular/wxsmall/WXSmallSellerController.java
@@ -0,0 +1,18 @@
+package com.soft.ware.rest.modular.wxsmall;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.soft.ware.core.base.controller.BaseController;
+@RestController
+public class WXSmallSellerController extends BaseController {
+	
+    @RequestMapping()
+	public Object getOpenId(String id){
+		return null;
+	}
+	
+	
+	
+	
+}
