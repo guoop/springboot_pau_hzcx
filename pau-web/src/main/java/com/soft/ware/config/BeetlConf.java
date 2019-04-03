a4261bb6d97b7846b7c2b4dc32e7c54a24c16193
paulo <744964089@qq.com>
2019-04-03 20:10:36 +0800

    新建web端

---
+++ pau-web/src/main/java/com/soft/ware/config/BeetlConf.java
@@ -0,0 +1,72 @@
+package com.soft.ware.config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.beetl.core.resource.ClasspathResourceLoader;
+import org.beetl.ext.spring.BeetlGroupUtilConfiguration;
+import org.beetl.ext.spring.BeetlSpringViewResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+@Configuration
+public class BeetlConf {
+
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Bean(name = "beetlConfig")
+    public BeetlGroupUtilConfiguration getBeetlGroupUtilConfiguration() {
+        BeetlGroupUtilConfiguration beetlGroupUtilConfiguration = new BeetlGroupUtilConfiguration();
+        ClasspathResourceLoader classpathResourceLoader = new ClasspathResourceLoader();
+        beetlGroupUtilConfiguration.setResourceLoader(classpathResourceLoader);
+        beetlGroupUtilConfiguration.init();
+        return beetlGroupUtilConfiguration;
+    }
+    @Bean(name = "beetlViewResolver")
+    public BeetlSpringViewResolver getBeetlSpringViewResolver(
+            @Qualifier("beetlConfig") BeetlGroupUtilConfiguration beetlGroupUtilConfiguration) {
+        BeetlSpringViewResolver beetlSpringViewResolver = new BeetlSpringViewResolver();
+        beetlSpringViewResolver.setPrefix("/templates/");
+        beetlSpringViewResolver.setSuffix(".html");
+        beetlSpringViewResolver.setContentType("text/html;charset=UTF-8");
+        beetlSpringViewResolver.setOrder(0);
+        beetlSpringViewResolver.setConfig(beetlGroupUtilConfiguration);
+        return beetlSpringViewResolver;
+    }
+
+   /* //=============  以下是beetsql配置  =============
+    @Bean(name = "beetlSqlScannerConfigurer")
+    public BeetlSqlScannerConfigurer getBeetlSqlScannerConfigurer() {
+        BeetlSqlScannerConfigurer conf = new BeetlSqlScannerConfigurer();
+        conf.setBasePackage("com.example.demobeetl2.dao");
+        conf.setDaoSuffix("Dao");
+        conf.setSqlManagerFactoryBeanName("sqlManagerFactoryBean");
+        return conf;
+    }*/
+
+
+
+    //配置数据库
+    @Bean(name = "datasource")
+    public DataSource getDataSource(Environment env) {
+        String url = env.getProperty("spring.datasource.url");
+        String userName = env.getProperty("spring.datasource.username");
+        String password = env.getProperty("spring.datasource.password");
+        return (DataSource) DataSourceBuilder.create().url(url).username(userName).password(password).build();
+    }
+    //开启事务
+    @Bean(name = "transactionManager")
+    public DataSourceTransactionManager getDataSourceTransactionManager(@Qualifier("datasource") DataSource datasource) {
+        DataSourceTransactionManager dsm = new DataSourceTransactionManager();
+        dsm.setDataSource(datasource);
+        return dsm;
+    }
+
+
+}
