8f1e5ab1918e3779d9e780229672447e798af2c6
javaccy <javaccy@163.com>
2019-04-09 20:42:07 +0800

    添加商品列表，商铺信息接口

--- pau-rest/src/main/java/com/soft/ware/rest/modular/wx_app/service/ISWxAppService.java
+++ pau-rest/src/main/java/com/soft/ware/rest/modular/wx_app/service/ISWxAppService.java
@@ -1,6 +1,7 @@
 package com.soft.ware.rest.modular.wx_app.service;
+
 import com.baomidou.mybatisplus.service.IService;
-import com.soft.ware.rest.common.persistence.model.TblOwner;
+import com.soft.ware.rest.modular.owner.model.TOwner;
 import com.soft.ware.rest.modular.wx_app.model.SWxApp;
 
 import java.util.List;
@@ -10,7 +11,7 @@ public interface ISWxAppService extends IService<SWxApp> {
 
     SWxApp findByAppId(String appId);
 
-    SWxApp find(TblOwner owner);
+    SWxApp find(TOwner owner);
 
     List<Map<String,Object>> find(Map<String,Object> map);
 
