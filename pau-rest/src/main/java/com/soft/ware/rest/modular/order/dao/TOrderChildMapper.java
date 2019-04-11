c0d4cce600fcfdf05f79310b99580dd61b08613f
paulo <744964089@qq.com>
2019-04-11 20:52:32 +0800

    生成基础子订单逻辑

---
+++ pau-rest/src/main/java/com/soft/ware/rest/modular/order/dao/TOrderChildMapper.java
@@ -0,0 +1,16 @@
+package com.soft.ware.rest.modular.order.dao;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.soft.ware.rest.modular.order.model.TOrderChild;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author paulo123
+ * @since 2019-04-11
+ */
+public interface TOrderChildMapper extends BaseMapper<TOrderChild> {
+
+}
